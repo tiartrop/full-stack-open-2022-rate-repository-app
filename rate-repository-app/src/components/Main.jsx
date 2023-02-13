df170a9c81d25269d7928f9d8f7c96efd5e24076
tiartrop <achessend@gmail.com>
2023-02-13 20:42:03 +0800

    init api

---
+++ rate-repository-app/src/components/Main.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, View } from "react-native";
+import { Route, Routes, Navigate } from "react-router-native";
+
+import AppBar from "./AppBar";
+import RepositoryList from "./RepositoryList";
+import SignIn from "./SignIn";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    flexShrink: 1,
+    backgroundColor: theme.backgroundColor.mainBackground
+  }
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="/login" element={<SignIn />} exact />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </View>
+  );
+};
+
+export default Main;
